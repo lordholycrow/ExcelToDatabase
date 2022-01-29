1000c3bf694be37f6d6817b6363c7052e84abb2d
Muhammed Ali Sarimese <56533860+lordholycrow@users.noreply.github.com>
2022-01-29 15:14:13 +0100

    Add files via upload

---
+++ Exporttst/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace Exporttst
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtDosyaYolu = new System.Windows.Forms.TextBox();
+            this.btnGozat = new System.Windows.Forms.Button();
+            this.btnAktar = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtDosyaYolu
+            // 
+            this.txtDosyaYolu.Location = new System.Drawing.Point(29, 106);
+            this.txtDosyaYolu.Name = "txtDosyaYolu";
+            this.txtDosyaYolu.Size = new System.Drawing.Size(298, 20);
+            this.txtDosyaYolu.TabIndex = 3;
+            // 
+            // btnGozat
+            // 
+            this.btnGozat.Location = new System.Drawing.Point(343, 100);
+            this.btnGozat.Name = "btnGozat";
+            this.btnGozat.Size = new System.Drawing.Size(68, 30);
+            this.btnGozat.TabIndex = 4;
+            this.btnGozat.Text = "Gözat";
+            this.btnGozat.UseVisualStyleBackColor = true;
+            this.btnGozat.Click += new System.EventHandler(this.btnGozat_Click);
+            // 
+            // btnAktar
+            // 
+            this.btnAktar.Location = new System.Drawing.Point(29, 147);
+            this.btnAktar.Name = "btnAktar";
+            this.btnAktar.Size = new System.Drawing.Size(360, 57);
+            this.btnAktar.TabIndex = 5;
+            this.btnAktar.Text = "Veri Tabanına Aktar";
+            this.btnAktar.UseVisualStyleBackColor = true;
+            this.btnAktar.Click += new System.EventHandler(this.btnAktar_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(29, 34);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(304, 21);
+            this.comboBox1.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Period";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(437, 226);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btnAktar);
+            this.Controls.Add(this.btnGozat);
+            this.Controls.Add(this.txtDosyaYolu);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtDosyaYolu;
+        private System.Windows.Forms.Button btnGozat;
+        private System.Windows.Forms.Button btnAktar;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Exporttst/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+
+namespace Exporttst
+{
+    public partial class Form1 : Form
+    {
+        DataTable dt = new DataTable();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGozat_Click(object sender, EventArgs e)
+        {
+            //////////////////////////////////////////////////
+            openFileDialog1.ShowDialog();
+            txtDosyaYolu.Text = openFileDialog1.FileName;
+            string dosyaYolu = openFileDialog1.FileName;
+            OleDbConnection baglantii = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0; Data Source=" + dosyaYolu + ";Extended Properties=Excel 12.0");
+            baglantii.Open();
+            string sorgu = "select * from [Sayfa1$]";
+            OleDbDataAdapter data_adaptor = new OleDbDataAdapter(sorgu, baglantii);
+            baglantii.Close();
+           
+            data_adaptor.Fill(dt);
+            //////////////////////////////////////////////////
+        }
+
+
+
+
+        private void btnAktar_Click(object sender, EventArgs e)
+        {
+
+
+            SqlConnection baglan = new SqlConnection("Data Source=.; Initial Catalog=.; User Id=.; password=.");
+
+            
+
+
+            for (int i = 0; i < dt.Rows.Count - 1; i++)
+            {
+
+                //////////////////////////////////////////////////
+
+                SqlCommand cmd2 = new SqlCommand();
+                baglan.Open();
+                cmd2.Connection = baglan;
+
+                cmd2.CommandText = "select ProductId from  Product where ProductCode = '" + dt.Rows[i][0].ToString() + "'";
+                int productid = (int)cmd2.ExecuteScalar();
+                cmd2.Dispose();
+                baglan.Close();
+                //////////////////////////////////////////////////
+
+
+
+
+
+                //////////////////////////////////////////////////
+                SqlCommand cmd3 = new SqlCommand();
+                baglan.Open();
+                cmd3.Connection = baglan;
+                cmd3.CommandText = "select StartDate from  Period where Title = '" + comboBox1.Text + "'";              
+                DateTime startdate = (DateTime)cmd3.ExecuteScalar();
+                cmd3.Dispose();
+                baglan.Close();
+                //////////////////////////////////////////////////
+                //////////////////////////////////////////////////
+                SqlCommand cmd8 = new SqlCommand();
+                baglan.Open();
+                cmd8.Connection = baglan;
+                cmd8.CommandText = "select Enddate from  Period where Title = '" + comboBox1.Text + "'";
+                DateTime Enddate = (DateTime)cmd8.ExecuteScalar();
+                cmd8.Dispose();
+                baglan.Close();
+                //////////////////////////////////////////////////
+
+
+                //////////////////////////////////////////////////
+                int CurrencyType = 53;
+                int CompanyId = 2;
+                string CreatedDate = "2010 - 10 - 10 00:00:00.000";
+                string CreatedUser = "Muhammed";
+                int s = (Byte)1;
+                //////////////////////////////////////////////////
+
+                //////////////////////////////////////////////////
+                SqlCommand cmd = new SqlCommand();
+                baglan.Open();
+                cmd.Connection = baglan;
+                cmd.CommandText = "insert into ProductPrice(ProductId,CompanyId,DbRow3,DbRow4,DbRow5,DbRow6) values(@ProductId,@CompanyId,@DbRow3,@DbRow4,@DbRow5,@DbRow6)";
+                cmd.Parameters.Add("@CompanyId", CompanyId);
+                cmd.Parameters.Add("@ProductId", productid);
+                cmd.Parameters.Add("@DbRow2", CurrencyType);
+                cmd.Parameters.Add("@DbRow3", decimal.Parse(dt.Rows[i][2].ToString()));
+                cmd.Parameters.Add("@DbRow4", decimal.Parse(dt.Rows[i][3].ToString()));
+                cmd.Parameters.Add("@DbRow5", decimal.Parse(dt.Rows[i][4].ToString()));
+       
+                cmd.ExecuteNonQuery();
+                cmd.Dispose();
+                baglan.Close();
+                //////////////////////////////////////////////////
+
+
+                //////////////////////////////////////////////////
+                SqlCommand cmd5 = new SqlCommand();
+                baglan.Open();
+                cmd5.Connection = baglan;
+                cmd5.CommandText = "UPDATE Product set DbRow4=@DbRow4, DbRow3=@DbRow3,DbRow6=@DbRow6  where ProductId = '" + productid + "'";                   
+                cmd5.Parameters.Add("@DbRow4", decimal.Parse(dt.Rows[i][7].ToString()));
+                cmd5.Parameters.Add("@DbRow3", decimal.Parse(dt.Rows[i][8].ToString()));
+                cmd5.Parameters.Add("@DbRow6", decimal.Parse(dt.Rows[i][9].ToString()));
+                cmd5.ExecuteNonQuery();
+                cmd.Dispose();
+                baglan.Close();
+                //////////////////////////////////////////////////
+
+
+            }
+
+            MessageBox.Show("İşleminiz Başarıyla gerçekleşmiştir");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //////////////////////////////////////////////////
+            SqlConnection baglanti = new SqlConnection();
+            baglanti.ConnectionString = "Data Source=.; Initial Catalog=.; User Id=.; password=.";
+            SqlCommand komut = new SqlCommand();
+            komut.CommandText = "SELECT * FROM Period";
+            komut.Connection = baglanti;
+            komut.CommandType = CommandType.Text;
+
+            SqlDataReader dr;
+            baglanti.Open();
+            dr = komut.ExecuteReader();
+            while (dr.Read())
+            {
+                comboBox1.Items.Add(dr["Title"]);
+            }
+            baglanti.Close();
+            //////////////////////////////////////////////////
+        }
+
+        private void dtGoster_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
+        
